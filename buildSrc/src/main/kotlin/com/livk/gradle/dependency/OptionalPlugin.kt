1b7d3df5bbd586addf9adf6f45518b81a44c7630
livk <1375632510@qq.com>
2022-08-12 12:22:49 +0800

    feat: optional添加

---
+++ buildSrc/src/main/kotlin/com/livk/gradle/dependency/OptionalPlugin.kt
@@ -0,0 +1,35 @@
+package com.livk.gradle.dependency
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPlugin
+import org.gradle.api.plugins.JavaPluginExtension
+
+/**
+ * <p>
+ * OptionalPlugin
+ * </p>
+ *
+ * @author livk
+ * @date 2022/8/12
+ */
+abstract class OptionalPlugin : Plugin<Project> {
+    companion object {
+        const val OPTIONAL = "optional"
+    }
+
+    override fun apply(project: Project) {
+        val configurations = project.configurations
+        project.pluginManager.apply(JavaPlugin::class.java)
+        configurations.create(OPTIONAL) { optional ->
+            optional.isCanBeResolved = false
+            optional.isCanBeConsumed = false
+            project.plugins.withType(JavaPlugin::class.java) {
+                project.extensions.getByType(JavaPluginExtension::class.java).sourceSets.all {
+                    configurations.getByName(it.compileClasspathConfigurationName).extendsFrom(optional)
+                    configurations.getByName(it.runtimeClasspathConfigurationName).extendsFrom(optional)
+                }
+            }
+        }
+    }
+}
